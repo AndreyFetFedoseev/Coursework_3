a27d9c0ff62866bcea858ae012ea14a057bb20fb
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-02-05 23:59:04 +0700

    changes class operations

--- .venv/operation.py
+++ .venv/operation.py
@@ -1,8 +1,7 @@
-import datetime
-
 class Operation:
 
-    def __init__(self, id='fetid', state='fetstate', date1='fetdate', operationAmount='fetoA', description='fetd', from1='fetfrom', to='fetto'):
+    def __init__(self, id='fetid', state='fetstate', date1='fetdate', operationAmount='fetoA', description='fetd',
+                 from1='fetfrom', to='fetto'):
         self.id = id
         self.state = state
         self.date1 = date1
@@ -11,7 +10,6 @@ class Operation:
         self.from1 = from1
         self.to = to
 
-
     def date_conversion(self):
         date_slice = self.date1[:10]
         date_list = date_slice.split('-')
@@ -19,26 +17,7 @@ class Operation:
         date_conversion = '.'.join(date_list)
         return date_conversion
 
-
-    def description_conversion(self):
-        return self.description
-
-
     def from_conversion(self):
-        # i = 1
-        # elements = []
-        # for element in self.from1:
-        #     print(element)
-        #     if element.isalpha():
-        #         continue
-        #     elif 0 < element.isdigit() < 18:
-        #         if 6 < i < 13:
-        #             element = '*'
-        #         i += 1
-        #     elif element.isdigit() > 18:
-        #         element = '**' + element[:-4]
-        #     elements.append(element)
-        # return ''.join(elements)
         elements = []
         list_from = self.from1.split(' ')
         for element in list_from:
@@ -53,18 +32,11 @@ class Operation:
                     elements.append(elemen)
         return ' '.join(elements)
 
-
     def to_conversion(self):
         elements = []
         list_to = self.to.split(' ')
         for element in list_to:
             if element.isdigit():
-            #     elements.append(element)
-            # else:
-            #     if len(element) < 18:
-            #         elem = element[:6] + len(element[6:-4]) * '*' + element[-4:]
-            #         elements.append(elem)
-            #     else:
                 element = '**' + element[-4:]
             elements.append(element)
         return ' '.join(elements)
