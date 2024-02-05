26cad60cdbfea662ceaf1af83e80e9090e19d59a
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-02-05 23:55:13 +0700

    create list class operations

--- .venv/functions.py
+++ .venv/functions.py
@@ -1,23 +1,15 @@
 import json
 from operation import Operation
 
+
 def load_list_operations(file_name):
     """
-    Получаем список вопросов из файла.json
+    Получаем список операций из файла.json
     :param file_name:
     :return:
     """
     with open(file_name, 'r', encoding='utf-8') as file:
         return json.load(file)
-
-
-# def sorted_last_five(list_file):
-#     for dict_operation in list_file:
-#
-#         last_sort_five = sorted(list_file, key=lambda x: x['date'])
-#     return last_sort_five
-def get_name(dictionary):
-    return dictionary['date']
 
 
 def load_operation(list_dict):
@@ -28,28 +20,22 @@ def load_operation(list_dict):
             operations.append(dict_operation)
     return operations
 
+
 list_oper = load_list_operations('operations.json')
-# print(list_oper)
 list_operations_executed = load_operation(list_oper)
-# operations = []
-# for dict_ in list_oper:
-#     operations.append(Operation(id=dict_['id'], state=dict_['state'], date=dict_['date'], operationAmount=dict_['operationAmount'], description=dict_['description'], from1='none', to=dict_['to']))
-# # list_oper.sort(key=get_name)
-# sorted(operations, key=lambda x: x[Operation.date])
-# print(list_oper[1].get('date'))
 sort_list_operations = sorted(list_operations_executed, key=lambda x: x.get("date"))
 list_last_opers = sort_list_operations[-5:]
 list_last_opers.reverse()
-# for operation in list_oper:
-#     sorted(list_oper, key=operation.get('date'))
-# print(list_oper)
 print(list_last_opers)
-for dict_ in list_last_opers[:1]:
-    oper_1 = (Operation(id=dict_['id'], state=dict_['state'], date1=dict_['date'], operationAmount=dict_['operationAmount'], description=dict_['description'], from1=dict_.get('from', 'Неизвестно'), to=dict_['to']))
-    # operat.append(oper_1)
-print(oper_1.date_conversion())
-print(oper_1.from_conversion())
-print(oper_1.to_conversion())
-print(oper_1.description)
-print(oper_1.operationAmount['amount'])
-print(oper_1.operationAmount['currency']['name'])
+operat = []
+for dict_ in list_last_opers:
+    oper_1 = (
+        Operation(id=dict_['id'], state=dict_['state'], date1=dict_['date'], operationAmount=dict_['operationAmount'],
+                  description=dict_['description'], from1=dict_.get('from', 'Неизвестно'), to=dict_['to']))
+    operat.append(oper_1)
+print(operat)
+for operation in operat:
+    print(f'{operation.date_conversion()}  {operation.description}\n'
+          f'{operation.from_conversion()} -> {operation.to_conversion()}\n'
+          f'{operation.operationAmount['amount']} {operation.operationAmount['currency']['name']}\n')
+
